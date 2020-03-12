91d2ef10e50482f5668223ccbc5bb6b98d2c5811
rustychaiNZ <rusljohn@gmail.com>
2020-03-13 10:42:11 +1300

    changed the user.js mdoel

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-	_id : mongoose.Schema.Types.ObjectId, 
+	user_id : mongoose.Schema.Types.ObjectId, 
 	username : String ,
 	email : String ,
 	password : String 
